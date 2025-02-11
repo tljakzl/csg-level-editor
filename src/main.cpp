46d2ca29a4b0e6137688331807d44590a75be612
Anatoliy Kozlov <tljakzl2@gmail.com>
2025-02-11 17:40:19 +0700

    promt 3

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 
 //#define CGAL_USE_BASIC_VIEWER
+#define GLM_ENABLE_EXPERIMENTAL
 
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
@@ -11,6 +12,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Surface_mesh.h>
 //#include <CGAL/draw_surface_mesh.h>
@@ -35,6 +37,7 @@ public:
     GLuint vao = 0, vbo = 0;
     glm::mat4 transform = glm::mat4(1.0f);
     glm::vec3 color = glm::vec3(1.0f);
+    bool visible = true;
 
     void uploadToGPU() {
         std::vector<float> vertexData;
@@ -150,11 +153,33 @@ public:
 // Класс объекта сцены
 class SceneObject {
 public:
+    // Добавить в класс SceneObject
+    struct Transform {
+        glm::vec3 position{ 0.0f };
+        glm::quat m_orientation;
+        glm::vec3 rotation{ 0.0f }; // В градусах
+        glm::vec3 scale{ 1.0f };
+
+        glm::mat4 matrix() const {
+            glm::mat4 m = glm::mat4(1.0f);
+            m = glm::translate(m, position);
+            m = glm::rotate(m, glm::radians(rotation.x), glm::vec3(1, 0, 0));
+            m = glm::rotate(m, glm::radians(rotation.y), glm::vec3(0, 1, 0));
+            m = glm::rotate(m, glm::radians(rotation.z), glm::vec3(0, 0, 1));
+            m = glm::scale(m, scale);
+            return m;
+        }
+    };
+public:
     Mesh mesh;
+    Transform transform;
     bool selected = false;
     bool isOperationResult = false; // Для автоматически созданных объектов
 
     SceneObject(Mesh&& m) : mesh(std::move(m)) {}
+    void updateTransform() {
+        mesh.transform = transform.matrix();
+    }
 };
 
 // Менеджер сцены
@@ -263,7 +288,51 @@ private:
 GLFWwindow* initGLFW();
 void initImGui(GLFWwindow* window);
 void createCube(Mesh& mesh, float size = 1.0f);
-void handleGizmo(Mesh& mesh, const glm::mat4& view, const glm::mat4& proj);
+void handleGizmo(SceneObject& obj, const glm::mat4& view, const glm::mat4& proj);
+
+// Новое окно свойств
+void drawPropertiesWindow(Scene& scene) {
+    ImGui::Begin("Object Properties");
+
+    if (scene.selectedObject) {
+        SceneObject& obj = *scene.selectedObject;
+
+        // Позиция
+        if (ImGui::DragFloat3("Position", glm::value_ptr(obj.transform.position), 0.1f)) {
+            obj.updateTransform();
+        }
+
+        // Поворот
+        glm::vec3 rotation = obj.transform.rotation;
+        if (ImGui::DragFloat3("Rotation", glm::value_ptr(rotation), 1.0f, -180.0f, 180.0f)) {
+            obj.transform.rotation = rotation;
+            obj.updateTransform();
+        }
+
+        // Масштаб
+        if (ImGui::DragFloat3("Scale", glm::value_ptr(obj.transform.scale), 0.1f, 0.01f, 10.0f)) {
+            obj.updateTransform();
+        }
+
+        // Цвет
+        ImGui::ColorEdit3("Color", glm::value_ptr(obj.mesh.color));
+
+        // Дополнительные параметры
+        ImGui::Separator();
+        if (ImGui::Button("Reset Transform")) {
+            obj.transform = SceneObject::Transform();
+            obj.updateTransform();
+        }
+
+        ImGui::SameLine();
+        ImGui::Checkbox("Visible", &obj.mesh.visible);
+    }
+    else {
+        ImGui::Text("Select an object to edit properties");
+    }
+
+    ImGui::End();
+}
 
 int main() {
     GLFWwindow* window = initGLFW();
@@ -333,6 +402,9 @@ int main() {
         }
         ImGui::End();
 
+        // Окно свойств
+        drawPropertiesWindow(scene);
+
         // Окно вьюпорта
         ImGui::Begin("Viewport", nullptr, 
             ImGuiWindowFlags_NoScrollbar | 
@@ -356,7 +428,7 @@ int main() {
         // Рендеринг
         renderScene(proj * view);
         if (scene.selectedObject) {
-            handleGizmo(scene.selectedObject->mesh, view, proj);
+            handleGizmo(*scene.selectedObject, view, proj);
         }
         
         ImGui::End();
@@ -431,19 +503,41 @@ void createCube(Mesh& mesh, float size) {
 }
 
 // Работа с Gizmo
-void handleGizmo(Mesh& mesh, const glm::mat4& view, const glm::mat4& proj) {
+void handleGizmo(SceneObject& obj, const glm::mat4& view, const glm::mat4& proj) {
     static ImGuizmo::OPERATION op = ImGuizmo::TRANSLATE;
-    
+    static ImGuizmo::MODE mode = ImGuizmo::WORLD;
+
+    if (ImGui::IsKeyPressed(ImGuiKey_W)) op = ImGuizmo::TRANSLATE;
+    if (ImGui::IsKeyPressed(ImGuiKey_E)) op = ImGuizmo::ROTATE;
+    if (ImGui::IsKeyPressed(ImGuiKey_R)) op = ImGuizmo::SCALE;
+
     ImGuizmo::SetOrthographic(false);
-    ImGuizmo::SetRect(0, 0, 
-        ImGui::GetIO().DisplaySize.x, 
+    ImGuizmo::SetRect(0, 0,
+        ImGui::GetIO().DisplaySize.x,
         ImGui::GetIO().DisplaySize.y);
-    
+
+    glm::mat4 delta;
     ImGuizmo::Manipulate(
         glm::value_ptr(view),
         glm::value_ptr(proj),
         op,
-        ImGuizmo::WORLD,
-        glm::value_ptr(mesh.transform)
+        mode,
+        glm::value_ptr(obj.mesh.transform),
+        glm::value_ptr(delta)
     );
+
+    // Обновляем Transform из матрицы
+    if (ImGuizmo::IsUsing()) {
+        glm::vec3 skew;
+        glm::vec4 perspective;
+        glm::decompose(
+            obj.mesh.transform,
+            obj.transform.scale,
+            obj.transform.m_orientation,
+            obj.transform.position,
+            skew,
+            perspective
+        );
+        obj.transform.rotation = glm::degrees(glm::eulerAngles(obj.transform.m_orientation));
+    }
 }
